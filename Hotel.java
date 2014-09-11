042401255183a4fd55b7c0d0db9428a9f5db18dd
EamonnOB <eamonn.ob@hotmail.com>
2014-09-11 14:47:11 +0100

    Update Hotel.java

--- Hotel.java
+++ Hotel.java
@@ -1,29 +1,46 @@
 import java.io.*;
 import java.util.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import javax.swing.JOptionPane;
 
 public class Hotel {
 	//name;arrival date;departure date;breakfast included(0,1);dinner included(0,1);room type(0,1,2);num of guests;price paid;bill paid(0,1)
 	public static void main(String[] args) throws IOException {
-			
-		String message = "Welcome to the Imaginary Inn!\nWhat would you like to do?";
-		Object[] menuOptions = {"View Guests"};
+		ArrayList<Guest> guests = getGuestsFromFile();
+		ArrayList<Guest> pastGuests = getPastGuests(guests);
+		//ArrayList<Guest> currentGuests = getCurrentGuests(guests);
+		ArrayList<Guest> futureBookings = getFutureBookings(guests);
+		String input = "";
+		String message = "Welcome to the Imaginary Inn!\nWhat would you like to do?\n\n\n";
+		Object[] menuOptions = {"View Past Guests","View Current Guests","View Future Bookings","Make Booking","Cancel Booking"};
 		int choice = 0;
 		while(choice != -1) {
 			choice = JOptionPane.showOptionDialog(null, message, "Menu",0,3,null,menuOptions,menuOptions[0]);
-			if(choice == 0) {
-				StringBuilder sb = new StringBuilder("Guests\n");
-				ArrayList<Guest> guests = getGuestsFromFile();
-				for(int g=0; g<guests.size(); g++) {
-					sb.append((g+1) + guests.get(g).getGuestName() + "\n");
+			switch(choice) {
+				case 0: {
+					input = "";
+					StringBuilder sb = new StringBuilder("Past Guests\n");
+					for(int g=0; g<pastGuests.size(); g++) {
+						sb.append((g+1) + " " + pastGuests.get(g).getGuestName() + "\n");
+					}
+					while(input != null && !(input.matches("\\d+"))) {
+						input = JOptionPane.showInputDialog(null,sb.toString()+"\nEnter a number to get information on a guest","Menu",1);
+					}	
 				}
-				String input = "";
-				while(input != null && !(input.matches("[0-9]{2}"))) {
-					input = JOptionPane.showInputDialog(null,sb.toString()+"\nEnter a number to get information on guest","Menu",1);
-				}
+				break;
+				case 2: {
+					input = "";
+					StringBuilder sb = new StringBuilder("Future Bookings\n");
+					for(int g=0; g<futureBookings.size(); g++) {
+						sb.append((g+1) + " " + futureBookings.get(g).getGuestName() + "\n");
+					}
+					while(input != null && !(input.matches("\\d+"))) {
+						input = JOptionPane.showInputDialog(null,sb.toString()+"\nEnter a number to get information on a guest","Menu",1);
+					}	
+				}	
 			}
 		}
-		
 	}
 	
 	public static ArrayList<Guest> getGuestsFromFile() throws IOException {
@@ -59,6 +76,61 @@ public class Hotel {
             out.println(list.get(i));
         }
         out.close();
+    }
+    
+    public static String getCurrentDate() {
+	    
+	 	Date d = new Date();
+		DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+		return df.format(d);
+	       
+    }
+    
+    public static ArrayList<Guest> getPastGuests(ArrayList<Guest> guests) {
+	    ArrayList<Guest> pastGuests = new ArrayList<Guest>();
+	    String currentDate = getCurrentDate();
+	    String[] splitCurrentDate = currentDate.split("/");
+	    for(int x=0; x<guests.size(); x++) {
+		    String[] departureDate = guests.get(x).getDepartureDate().split("/");
+		    if((splitCurrentDate[2].compareTo(departureDate[2])) <= 0) {
+			 	if((splitCurrentDate[2].compareTo(departureDate[2])) < 0)
+			 		pastGuests.add(guests.get(x));
+			 	else if((splitCurrentDate[1].compareTo(departureDate[1])) <= 0) {
+				 	if((splitCurrentDate[1].compareTo(departureDate[1])) < 0)
+			 			pastGuests.add(guests.get(x));
+			 		else if((splitCurrentDate[0].compareTo(departureDate[0])) < 0)
+			 			pastGuests.add(guests.get(x));
+			 	}	
+	    	}
+	    }
+	    return pastGuests;
+    }
+    
+    /*public static ArrayList<Guest> getCurrentGuests(ArrayList<Guest> guests) {
+	    String currentDate = getCurrentDate();
+	    for(int y=0; y<guests.size(); y++) {
+		    
+	    }
+    }*/
+    
+    public static ArrayList<Guest> getFutureBookings(ArrayList<Guest> guests) {
+	    ArrayList<Guest> futureGuests = new ArrayList<Guest>();
+	    String currentDate = getCurrentDate();
+	    String[] splitCurrentDate = currentDate.split("/");
+	    for(int z=0; z<guests.size(); z++) {
+		    String[] arrivalDate = guests.get(z).getArrivalDate().split("/");
+		    if((splitCurrentDate[2].compareTo(arrivalDate[2])) >= 0) {
+			 	if((splitCurrentDate[2].compareTo(arrivalDate[2])) > 0)
+			 		futureGuests.add(guests.get(z));
+			 	else if((splitCurrentDate[1].compareTo(arrivalDate[1])) >= 0) {
+				 	if((splitCurrentDate[1].compareTo(arrivalDate[1])) > 0)
+			 			futureGuests.add(guests.get(z));
+			 		else if((splitCurrentDate[0].compareTo(arrivalDate[0])) > 0)
+			 			futureGuests.add(guests.get(z));
+			 	}	
+	    	}
+	    }
+	    return futureGuests;
     }
 	
 }
